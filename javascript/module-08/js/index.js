0ddf0ca907630703a396739fdd93c4a6d7957d6f
kl1m <kl1m.done@gmail.com>
2018-06-19 17:41:25 +0300

    adds 1.1v

--- javascript/module-08/js/index.js
+++ javascript/module-08/js/index.js
@@ -4,38 +4,45 @@ const galleryItems = [
   {
     preview: "img/tech01-pre.jpeg",
     fullview: "img/tech01-full.jpeg",
-    alt: "technology picture"
+    alt: "technology picture #1"
   },
   {
     preview: "img/tech02-pre.jpeg",
     fullview: "img/tech02-full.jpeg",
-    alt: "technology picture"
+    alt: "technology picture #2"
   },
   {
     preview: "img/tech03-pre.jpeg",
     fullview: "img/tech03-full.jpeg",
-    alt: "technology picture"
+    alt: "technology picture #3"
   },
   {
     preview: "img/tech04-pre.jpeg",
     fullview: "img/tech04-full.jpeg",
-    alt: "technology picture"
+    alt: "technology picture #4"
   },
   {
     preview: "img/tech05-pre.jpeg",
     fullview: "img/tech05-full.jpeg",
-    alt: "technology picture"
+    alt: "technology picture #5"
   },
   {
     preview: "img/tech06-pre.jpeg",
     fullview: "img/tech06-full.jpeg",
-    alt: "technology picture"
+    alt: "technology picture #6"
   }
 ];
 
 const imageGallery = document.querySelector(".js-image-gallery");
 
-function createImageGallary({fullview, alt }) {
+function createPreviewItem(arr) {
+  const newArr = arr.reduce((acc, obj) => acc + createPreviewImage(obj), []);
+  console.log(newArr);
+  return newArr;
+}
+
+
+function createImageGallary({ fullview, alt }) {
   const fullView = document.createElement("div");
   fullView.classList.add("fullview");
 
@@ -48,13 +55,13 @@ function createImageGallary({fullview, alt }) {
   const previewGallery = document.createElement("ul");
   previewGallery.classList.add("preview");
 
-  previewGallery.append(createPreviewImage());
+  previewGallery.append(createPreviewImage(galleryItems));
 
   imageGallery.append(fullView, previewGallery);
   return imageGallery;
 }
 
-function createPreviewImage({preview, fullview, alt }) {
+function createPreviewImage({ preview, fullview, alt }) {
   const previewItem = document.createElement("li");
 
   const previewImage = document.createElement("img");
@@ -64,9 +71,10 @@ function createPreviewImage({preview, fullview, alt }) {
 
   previewItem.appendChild(previewImage);
 
-  console.log(previewItem);
+  //console.log(previewItem);
   return previewItem;
 }
 
-console.log(createPreviewImage(galleryItems))
-console.log(createImageGallary(galleryItems))
+createImageGallary(galleryItems);
+createPreviewItem(galleryItems);
+createPreviewImage(galleryItems);
